45a546204d784af6917744fb614aced67cb6450a
Ruan Lucas Donino <ruan_donino@hotmail.com>
2024-09-02 18:48:27 -0300

    add data quality

--- Data_Quality/data_quality.py
+++ Data_Quality/data_quality.py
@@ -8,12 +8,12 @@ def read_df(spark, read_path):
     df_stock = spark.read.parquet(read_path)
     return df_stock
 
-def setup_context(spark, context_path):
-    context = gx.get_context(context_root_dir=context_path)
-    return context
+def setup_context(context_path):
+    context_out = gx.get_context(context_root_dir=context_path)
+    return context_out
 
 def create_data_source(df_stock, context):
-    data_source = context.sources.add_or_update_spark(
+    data_source = context.data_sources.add_or_update_spark(
         name="stock_data_in_memory",
     )
     data_asset = data_source.add_dataframe_asset(
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     read_path = f"gs://python_files_stock2/outputs_processed_data/processed_data_{today}"
     df_stock = read_df(spark, read_path)
     context_path = r".\context"
-    context = setup_context(spark, context_path)
+    context = setup_context(context_path)
     data_source, data_asset = create_data_source(df_stock, context)
     batch_request = data_asset.build_batch_request()
     expectations_suite_name = "data_quality_expectations"
